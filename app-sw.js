65181a1d36a43548c46913e76acc73f299e48efa
Vihang Patel <vihangpatel@Vihangs-MacBook-Pro.local>
2018-03-17 07:46:18 +0530

    Add offline support

--- app-sw.js
+++ app-sw.js
@@ -2,34 +2,76 @@
 	self.addEventListener("install", event => {
 		console.log("V1 installing…")
 
+		const cacheStoreName = `static-33038`
+		// Basically when code is built, new assets list is generated for client
+		// Which has associated cache
+
+		// event.waitUntil accepts promise
 		event.waitUntil(
-			caches.open("static-v1").then(cache => {
-				cache.add("/js/aboutus.js")
-				cache.add("/js/contactus.js")
-				cache.add("/js/home.js")
-				cache.add("/js/main.js")
-				cache.add("/js/product.js")
-				cache.add("/css/main.css")
+			caches.open(cacheStoreName).then(cache => {
+
+				// list of static assets from the assets map file
+				cache.addAll([
+					"/js/aboutus.js",
+					"/js/contactus.js",
+					"/js/home.js",
+					"/js/main.js",
+					"/js/product.js",
+					"/css/main.css",
+					"/css/offline.css",
+					"/offline.html"
+				])
 			})
 		)
 	})
 
 	self.addEventListener("activate", event => {
-		console.log("V1 now ready to handle fetches!")
+		// Initially when service worker is installed for first time, 
+		// request does not flow through service workers
+		// To flow the request through service workers, claim the client
+		clients.claim()	
+		console.log("Its now ready to handle fetches!")
+
+		event.waitUntil(
+			caches.keys().then(function(cacheNames) {
+				return Promise.all(
+					cacheNames
+						.filter(function(cacheName) {
+							// Return true if you want to remove this cache,
+							// but remember that caches are shared across
+							// the whole origin
+							console.log('cache to be removed : ' ,cacheName)
+							return true
+						})
+						.map(function(cacheName) {
+							return caches.delete(cacheName)
+						})
+				)
+			})
+		)
 	})
 
 	self.addEventListener("fetch", event => {
+		// if (event.request.method != "GET" || event.request.mode == "cors" || event.request.url.startsWith('chrome-extension')){
+		// 	event.respondWith(fetch(event.request))
+		// 	return
+		// } else { 
+		// 	return 
+		// }
+
 		event.respondWith(
 			caches.match(event.request).then(function(response) {
 				if (response) {
+					console.log("cached response : ", event.request)
 					return response
 				}
 				return fetch(event.request)
 					.then(function(fetchResponse) {
+						console.log("fetching : ", event.request)
 						return fetchResponse
 					})
 					.catch(function(error) {
-						throw error
+						return caches.match('/offline.html');
 					})
 			})
 		)
